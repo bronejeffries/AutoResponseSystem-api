3519be31c558bb644b3d15d48c7c9cccaebbc3d3
Brian Ssemate <ssematebrian2067@gmail.com>
2019-03-05 01:03:48 +0300

    initial changes2

--- ars_backend/settings.py
+++ ars_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '4fv_fo1$%n751%+90=g%_h549b)okcwiv(sy(&zib&2!oi-ht('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
