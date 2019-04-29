2a919f8aa0ebfb99ec5a67b6d2d20b48ba649004
Brian Ssemate <ssematebrian2067@gmail.com>
2019-04-29 21:18:47 +0300

    channels topic

--- moderator/signals.py
+++ moderator/signals.py
@@ -13,17 +13,21 @@ def announce_comment_create(sender, instance, created, **kwargs):
         channel_layer = get_channel_layer()
         topic = instance.topic
         presentation_name = topic.presentation_name
+        not_sent = True
         if presentation_name is not None or presentation_name is not "null":
-            try:
-                presentationViews.makeTopicpresentation(topic.id, name = presentation_name)
-                async_to_sync(channel_layer.group_send)(
-                    f"moderator_{presentation_name}",{
-                        "type":"moderator.announce",
-                        "event": "comment created"
-                    }
-                )
-            except ConnectionClosedError:
-                print("redis error on comments","connection closed")
+            while not_sent:
+                try:
+                    presentationViews.makeTopicpresentation(topic.id, name = presentation_name)
+                    async_to_sync(channel_layer.group_send)(
+                        f"moderator_{presentation_name}",{
+                            "type":"moderator.announce",
+                            "event": "comment created"
+                        }
+                    )
+                except ConnectionClosedError:
+                    print("redis error on comments","connection closed")
+                else:
+                    not_sent = False
     else:
         print("channel message not sent on comments........")
 
@@ -34,16 +38,21 @@ def announce_option_create(sender, instance, created, **kwargs):
         channel_layer = get_channel_layer()
         question = instance.question
         presentation_name = question.presentation_name
+        not_sent = True
         if presentation_name is not None:
-            try:
-                presentationViews.makeQuestionPresentation(question.id, name = presentation_name)
-                async_to_sync(channel_layer.group_send)(
-                    f"moderator_{presentation_name}",{
-                        "type":"moderator.announce",
-                        "event": "Options Update"
-                    }
-                )
-            except ConnectionClosedError:
-                print("redis error on options","connection closed")
+            while not_sent:
+                try:
+                    presentationViews.makeQuestionPresentation(question.id, name = presentation_name)
+                    async_to_sync(channel_layer.group_send)(
+                        f"moderator_{presentation_name}",{
+                            "type":"moderator.announce",
+                            "event": "Options Update"
+                        }
+                    )
+                except ConnectionClosedError:
+                    print("redis error on options","connection closed")
+                else:
+                    not_sent = False
+                    
     else:
         print("Channel message not sent on options::......")
